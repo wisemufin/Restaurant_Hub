d5469577328be3f66a2e9eb75eae316a0458ec47
unknown <erichung.94@gmail.com>
2019-04-05 14:15:11 -0500

    stash changes

--- server.js
+++ server.js
@@ -7,15 +7,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-var reservations = [
-  {
-    routeName: "",
-    name: "Mary",
-    phoneNumber: "123-456-7890",
-    email: "somebody@gmail.com",
-    uniqueID: "1"
-  }
-];
+var reservations = [];
+var waitlist = [];
 
 // Routes
 // ===========================================================
@@ -33,17 +26,28 @@ app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
+
 app.get("/api/tables", function(req, res) {
   return res.json(reservations);
 });
 
+
+if(reservations.length <= 5){
 app.post("/api/tables", function(req, res) {
   var newReservation = req.body;
   console.log(newReservation);
   reservations.push(newReservation);
   res.json(newReservation);
 });
-
+}
+else {
+app.post("/api/waitlist", function(req, res){
+    var newReservation = req.body; 
+    console.log("Waitlist:" + newReservation);
+    waitlist.push(newReservation);
+    res.json(newReservation);
+})
+}
 // Starts the server
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
